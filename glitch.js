7671a0743be4d02071af8e77240697a6fe8f3d3e
Nate Donato <natedonato@gmail.com>
2019-05-22 17:15:37 -0700

    can upload image and it will invert colors

---
+++ glitch.js
@@ -0,0 +1,46 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+var imageLoader = document.getElementById('uploadPhoto');
+imageLoader.addEventListener('change', handleImage, false);
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+function handleImage(e) {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        var img = new Image();
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+
+
+
+            
+            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            var i;
+            for (i = 0; i < imgData.data.length; i += 4) {
+                imgData.data[i] = 255 - imgData.data[i];
+                imgData.data[i + 1] = 255 - imgData.data[i + 1];
+                imgData.data[i + 2] = 255 - imgData.data[i + 2];
+                imgData.data[i + 3] = 255;
+            }
+            ctx.putImageData(imgData, 0, 0);
+
+
+
+
+
+        };
+        img.src = event.target.result;
+    };
+    reader.readAsDataURL(e.target.files[0]);
+    }
+
+
+
+});
+
+
