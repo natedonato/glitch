5b86198c50bbb91601ee862d739aecfe65be318e
Nate Donato <natedonato@gmail.com>
2019-05-22 18:08:05 -0700

    basic pixel sorter by red pixel

--- glitch.js
+++ glitch.js
@@ -21,12 +21,23 @@ function handleImage(e) {
             
             var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
             var i;
-            for (i = 0; i < imgData.data.length; i += 4) {
-                imgData.data[i] = 255 - imgData.data[i];
-                imgData.data[i + 1] = 255 - imgData.data[i + 1];
-                imgData.data[i + 2] = 255 - imgData.data[i + 2];
-                imgData.data[i + 3] = 255;
+
+            for(let j = 0; j< 100; j++){
+            for (i = 0; i < imgData.data.length-4; i += 4) {
+                if(imgData.data[i+4] < imgData.data[i]){
+                    imgData.data[i] = imgData.data[i+4];
+                    imgData.data[i+1] = imgData.data[i+4+1];
+                    imgData.data[i+2] = imgData.data[i+4+2];
+                    imgData.data[i+2] = imgData.data[i+4+2];
+                    imgData.data[i+3] = imgData.data[i+4+3];
+                    
+                // imgData.data[i] = 255 - imgData.data[i];
+                // imgData.data[i + 1] = 255 - imgData.data[i + 1];
+                // imgData.data[i + 2] = 255 - imgData.data[i + 2];
+                // imgData.data[i + 3] = 255;
+                }
             }
+        }
             ctx.putImageData(imgData, 0, 0);
 
 
