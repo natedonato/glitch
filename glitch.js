410b5ec2704d0db4c3e8270727e165e0ad82c818
Nate Donato <natedonato@gmail.com>
2019-05-22 18:32:16 -0700

    glitch effect on button press

--- glitch.js
+++ glitch.js
@@ -6,6 +6,8 @@ var imageLoader = document.getElementById('uploadPhoto');
 imageLoader.addEventListener('change', handleImage, false);
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var button = document.getElementById("glitch");
+button.addEventListener('click', glitch);
 
 function handleImage(e) {
     var reader = new FileReader();
@@ -15,42 +17,31 @@ function handleImage(e) {
             canvas.width = img.width;
             canvas.height = img.height;
             ctx.drawImage(img, 0, 0);
-
-
-
-            
-            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            var i;
-
-            for(let j = 0; j< 100; j++){
-            for (i = 0; i < imgData.data.length-4; i += 4) {
-                if(imgData.data[i+4] < imgData.data[i]){
-                    imgData.data[i] = imgData.data[i+4];
-                    imgData.data[i+1] = imgData.data[i+4+1];
-                    imgData.data[i+2] = imgData.data[i+4+2];
-                    imgData.data[i+2] = imgData.data[i+4+2];
-                    imgData.data[i+3] = imgData.data[i+4+3];
-                    
-                // imgData.data[i] = 255 - imgData.data[i];
-                // imgData.data[i + 1] = 255 - imgData.data[i + 1];
-                // imgData.data[i + 2] = 255 - imgData.data[i + 2];
-                // imgData.data[i + 3] = 255;
-                }
-            }
-        }
-            ctx.putImageData(imgData, 0, 0);
-
-
-
-
-
         };
         img.src = event.target.result;
     };
     reader.readAsDataURL(e.target.files[0]);
     }
 
+function glitch(){
+    console.log("hey");
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var i;
 
+    for (let j = 0; j < 100; j++) {
+        for (i = 0; i < imgData.data.length - 4; i += 4) {
+            if (imgData.data[i + 4] < imgData.data[i]) {
+                imgData.data[i] = imgData.data[i + 4];
+                imgData.data[i + 1] = imgData.data[i + 4 + 1];
+                imgData.data[i + 2] = imgData.data[i + 4 + 2];
+                imgData.data[i + 2] = imgData.data[i + 4 + 2];
+                imgData.data[i + 3] = imgData.data[i + 4 + 3];
+            }
+        }
+    }
+    ctx.putImageData(imgData, 0, 0);
+
+};
 
 });
 
