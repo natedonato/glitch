c23f83589be8db26671f445e3c5134056525901f
Nate Donato <natedonato@gmail.com>
2019-06-05 18:55:31 -0700

    directional glitching(DOESNT WORK ON NON SQUARE DIMENSIONS)

--- glitch.js
+++ glitch.js
@@ -9,7 +9,6 @@ var ctx = canvas.getContext('2d');
 var button = document.getElementById("glitch");
 var button2 = document.getElementById("glitch2");
 var getPhoto = document.getElementById("getPhoto");
-var getGlitchedPhoto = document.getElementById("getGlitchedPhoto");
 var downloadbutton = document.getElementById('download');
 
 downloadbutton.addEventListener('click', function (e) {
@@ -76,10 +75,39 @@ function glitch(){
     let x = document.getElementById("x").value;
     let percent = document.getElementById("percent");
     let brightness = document.getElementById("bright");
+    let direction = document.getElementById("direction").value;
     bright = !brightness.checked;
 
     percent.style.display = "block";
-    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var inMemoryCanvas = document.createElement('canvas');
+    inMemoryCanvas.width = canvas.width;
+    inMemoryCanvas.height = canvas.height;
+    ictx = inMemoryCanvas.getContext('2d');
+    inMemoryCanvas.width = canvas.height;
+    inMemoryCanvas.height = canvas.width;
+    ictx.translate(inMemoryCanvas.width / 2, inMemoryCanvas.height / 2);
+    
+    if(direction === "d"){
+        ictx.rotate(Math.PI / 2);
+    }else if (direction ==="u"){
+        ictx.rotate(-Math.PI / 2);
+    }else if (direction ==="r"){
+        ictx.rotate(Math.PI);
+    }
+    ictx.drawImage(canvas, -inMemoryCanvas.width / 2, -inMemoryCanvas.height / 2);
+    ictx.restore();
+
+
+
+    var imgData = ictx.getImageData(0, 0, canvas.width, canvas.height);
+
+
+    // if(direction === "u"){
+    //     ctx.rotate(Math.Pi / 2);
+    //     ctx.putImageData(imgData, 0, 0);
+    //     imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    // }
+
     var i;
     for (let j = 0; j < x; j++) {
         for (i = 0; i < imgData.data.length - 4; i += 4) {
@@ -100,7 +128,33 @@ function glitch(){
             }}
         }
     }
+
+    // if (direction === "u") {
+    //     ictx.clearRect(0,0,canvas.width, canvas.height)
+    //     ictx.putImageData(imgData, 0, 0);
+
+    // }
+
+    // ctx.drawImage(inMemoryCanvas, canvas.width, canvas.height);
+
     ctx.putImageData(imgData, 0, 0);
+
+    inMemoryCanvas.width = canvas.height;
+    inMemoryCanvas.height = canvas.width;
+    ictx.translate(inMemoryCanvas.width / 2, inMemoryCanvas.height / 2);
+    if (direction === "d") {
+        ictx.rotate(-Math.PI / 2);
+    }else if (direction ==="u"){
+        ictx.rotate(Math.PI / 2);
+    }else if (direction === "r") {
+        ictx.rotate(-Math.PI);
+    }
+    ictx.drawImage(canvas, -inMemoryCanvas.width / 2, -inMemoryCanvas.height / 2);
+    ictx.restore();
+
+    imgData = ictx.getImageData(0, 0, canvas.width, canvas.height);
+    ctx.putImageData(imgData, 0, 0);
+
     // gif.addFrame(canvas);
 }
 
